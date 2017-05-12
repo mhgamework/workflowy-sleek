be612ba6d19b0a23b6ed3eeaa077f4acd12a373f
Michiel Huygen <mhgamework@gmail;com>
2017-05-12 11:39:47 +0200

    Added open-sprint button

--- workflowy.user.js
+++ workflowy.user.js
@@ -15,48 +15,26 @@
     GM_addStyle(GM_getResourceText("css"));
     $(".customMH").remove();
 
-    var theNew = $('<div class="customMH button"><div class="topBarButtonTextContainer">@status OR #open</div></div>');
+    function createButton(query){
+        var theNew = $('<div class="customMH button"><div class="topBarButtonTextContainer">'+query+'</div></div>');
 
-    theNew.insertAfter($("#savedViewHUDButton"));
-    theNew.click(function () {
-        var toSet = "@status OR #open";
-        if ($("#searchBox").val() == toSet) {
-            toSet = "";
-            theNew.toggleClass("active", false);
-        }
-        else {
-            theNew.toggleClass("active", true);
-        }
+        theNew.insertAfter($("#savedViewHUDButton"));
+        theNew.click(function () {
+            var toSet = query;
+            if ($("#searchBox").val() == toSet) {
+                toSet = "";
+                theNew.toggleClass("active", false);
+            }
+            else {
+                theNew.toggleClass("active", true);
+            }
 
-        $("#searchBox").val(toSet);
-        $("#searchBox").submit();
-    });
-    /*theNew.css("display","block");
-     theNew.css("float","left");*/
+            $("#searchBox").val(toSet);
+            $("#searchBox").submit();
+        });
+    }
 
-    /*  margin-left: -1px;
-     padding: 8px 0;
-     font-size: 13px;
-     text-align: center;
-     float: right;
-     border-bottom: none;
-     border-left: 1px solid #111;
-     border-right: 1px solid #111;
-     border-radius: 0;
-     background-color: #555;
-     position: relative;*/
-    /*
-     theNew.css("border-bottom","none");
-     theNew.css("border-left","1px solid #111");
-     theNew.css("border-right","1px solid #111");
-     theNew.css("position","relative");
-     theNew.css("background-color","#555");
-     theNew.css("text-align","center");
-     theNew.css("padding","8px 10px");
-     theNew.css("color","white");
-     theNew.css("font-family","'Helvetica Neue',Arial,sans-serif");
-     theNew.css("font-size","13px");*/
+    createButton("@status OR #open-sprint");
+    createButton("@status OR #open");
 
-
-    // Your code here...
 })();
