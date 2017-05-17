56f1a28758e48844f9567a5441f07160444d615e
Michiel Huygen <michiel@getsleek.co>
2017-05-17 14:56:28 +0200

    Added fancy images sidebar

--- workflowy.user.js
+++ workflowy.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Workflowy-sleek
 // @namespace    http://getsleek.co/
-// @version      0.3.0
+// @version      0.3.1
 // @description  Sleek customizations to workflowy
 // @author       MHGameWork
 // @match        https://*workflowy.com/*
@@ -37,33 +37,127 @@
     createButton("@status OR #open-sprint");
     createButton("@status OR #open");
 
+	var $imagebar = $("<div>")
+	.addClass("customMH")
+	.addClass("imageBar")
+	.css({
+		"position":"fixed",
+		"left": "1050px",
+		"top": "100px",
+		"width": "200px",
+		"height": "100px",
+		//"background-color": "white"
+	});
+	$("body").append($imagebar);
 	
 	
 	
-	function do_parseImg() {
-		$(this).nextAll(".content-img").remove();
-		var lines = $(this).text().split("\n");
-		var img_re = /^\!\[.*\]\((.+)\)$/;
+		
+		
+	var IMAGE_EXTENSIONS = [".png", ".jpg", ".jpeg", ".gif", ".bmp"];
+	//var magicCounter = 0;
+	var magicCounterGlobal = 0;
+	function createImageNodeAfterNode($node, imgSrc) {
+		var id = $node.parents(".project").first().attr("projectid");
+		var imgId = "mhImg"+id;
+		$("#"+imgId).remove();
+	  //if ($node.parent().find(".content-img").length === 0) {
+		var $div = $("<div>")
+					  .addClass("content-img")
+					  .css({
+						"display": "hidden"
+					  });
+		//$div.data("magic",magicCounter);
+		//magicCounter +=1;
+		
+		var $img = $("<img>")
+					  .addClass("mhImg"+magicCounterGlobal)
+					  .addClass("mhImg")
+					  .attr("src", imgSrc)
+					  .attr("id", imgId)
+					  .css({
+						"max-width": "100%",
+						"max-height": "350px",
+						//"position":"absolute"
+					  });
+		
+		
+		var $a = $("<a>")
+					//.attr("href","#img"+magicCounter);
+					
+		$a.append($img);
+		$imagebar.append($a);
+		
+		var $aPreview = $("<a>")
+					  //.attr("href", "#_")
+					  //.attr("id", "img"+magicCounter)
+					  .addClass("lightbox");
+		var $imgPreview = $("<img>")
+					  .attr("src", imgSrc);
 
-		for (var i = 0; i < lines.length; i++) {
-			var line = lines[i].trim();
-			var img = line.match(img_re);
-			if (img === null) {
-				continue;
-			}
-			console.log(i, img[1]);
-			$(this).after('<div class="content-img"><img src="' + img[1] + '"/></div>')
-		}
+		$aPreview.append($imgPreview);
+		$("body").append($aPreview);
+
+		
+		$a.click(function(){$aPreview.show();});
+		$aPreview.click(function(){$aPreview.hide();});
+		
+		//$node.after($div);
+	  //}
 	}
 
-	function parseImg() {
-		$("div.notes div.content").live("click keyup", do_parseImg);
-		$("div.notes div.content").each(do_parseImg);
-		$("#expandButton").live("click", function() {
-			$("div.notes div.content").each(do_parseImg);
-		});
+	function generateImagesForContentNode(node) {
+	  var $node = $(node);
+
+	  var text = $node.text();
+
+	  var markdownImageRegex = /\!\[.*\]\((.+)\)/;
+	  var matcher = text.match(markdownImageRegex);
+	  if (matcher !== null) {
+		var imgSrc = matcher[1];
+		createImageNodeAfterNode($node, imgSrc);
+	  }
+	}
+
+	function generateImagesForLinkNode(node) {
+	  var $node = $(node);
+
+	  var url = $node.text();
+	  var curExtension = url.substr(-4);
+	  if (_.contains(IMAGE_EXTENSIONS, curExtension) && $node.parent().text()[0] !== "!") {
+		createImageNodeAfterNode($node.parent(), url);
+	  }
+	}
+
+	function checkForChanges() {
+		magicCounterGlobal += 1;
+	  $("div.name div.content, div.notes div.content").each(function(i, node) {
+		generateImagesForContentNode(node);
+	  });
+
+	  $("div.name a.contentLink, div.notes a.contentLink").each(function(i, node) {
+		generateImagesForLinkNode(node);
+	  });
+	  console.log("Removing "+(magicCounterGlobal-1));
+	  $(".mhImg").filter(".mhImg"+(magicCounterGlobal-1)).remove();
+
+	  // TODO: These currently need to be in this order because otherwise when
+	  // there is a raw link  in the notes, it will be overwritten
 	};
 
-	$(window).bind("load hashchange", parseImg);
+	// When the page finishes loading
+	$(window).load(function () {
+		checkForChanges();
+	});
+
+	// When you change nodes
+	//window.onhashchange = checkForChanges;
+
+	// When you press any keyboard key
+	$(document).keydown(function(e) {
+	  setTimeout(function() {
+		checkForChanges();
+	  }, 250);
+	});
 	
 })();
